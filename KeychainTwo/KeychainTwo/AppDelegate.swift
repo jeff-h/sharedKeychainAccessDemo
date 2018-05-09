a28d827e88e32bf63ac5c87d2460c90f51a710ef
Jeff <jeff@marmaladesoul.com>
2018-05-09 21:46:21 +1200

    Initial commit.

---
+++ KeychainTwo/KeychainTwo/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  KeychainTwo
+//
+//  Created by Jeff Hanbury on 7/05/18.
+//  Copyright © 2018 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        
+        let keychain = Keychain(service: "serviceNameF", accessGroup: "38T4C62JZM.com.marmaladesoul.testGroup")
+        
+        let token = keychain["testItem"]
+        
+//        if token == nil {
+//            keychain["testItem"] = "abc123"
+//        }
+        
+        let msg = token ?? "nil"
+        let alert = NSAlert()
+        alert.messageText = "Token: " + msg
+        alert.runModal()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeychainTwo/KeychainTwo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeychainTwo
+//
+//  Created by Jeff Hanbury on 7/05/18.
+//  Copyright © 2018 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
